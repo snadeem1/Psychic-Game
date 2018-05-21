34a1f82735d4a4083889d3cb2dd2fd1a635f5ae9
Sitwat <sitwat.nadeem1@gmail.com>
2018-05-21 14:47:57 -0400

    2nd change

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessesSoFar = [];
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var updateGuessesLeft = function() {
+    document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+  };
+
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+    guessesSoFar.push(userGuess); 
+    updateGuessesLeft();
+
+    if (userGuess == computerGuess) {
+        wins++;
+        document.querySelector('#wins').innerHTML = "Wins: " + wins;
+        alert('You are a PSYCHIC!');
+        guessesLeft = 9; 
+        guessesSoFar.length = 0;
+    }
+    else if (guessesLeft == 0){
+        losses++;
+        document.querySelector('#losses').innerHTML = "Losses: " + losses;
+        alert('You are not a Psychic! Try again');
+        guessesLeft = 9; 
+        guessesSoFar.length = 0;
+        
+    }
+    else if (userGuess !== computerGuess){
+        guessesLeft--;
+        document.querySelector('#let').innerHTML = "Your Guesses so far: " +
+        guessesSoFar.join(', ');
+    }
+    
+}
