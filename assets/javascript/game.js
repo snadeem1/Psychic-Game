431c09dc1432aa79735438f29c68e9db04e0be7d
Sitwat <sitwat.nadeem1@gmail.com>
2018-05-23 16:20:31 -0400

    new changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,32 +8,46 @@ var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l
 var updateGuessesLeft = function() {
     document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
   };
+var newGuessesSoFar = function() {
+    document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessesSoFar.join(', ');
+        
+};
+
+var reset = function() {
+    guessesSoFar = [];
+        document.querySelector('#let').innerHTML = "Your Guesses so far: ";
+        guessesLeft = 9; 
+        guessesSoFar.length = 0;
+}
+updateGuessesLeft();
 
 document.onkeyup = function(event) {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
     guessesSoFar.push(userGuess); 
-    updateGuessesLeft();
+    
 
+    
     if (userGuess == computerGuess) {
         wins++;
         document.querySelector('#wins').innerHTML = "Wins: " + wins;
         alert('You are a PSYCHIC!');
-        guessesLeft = 9; 
-        guessesSoFar.length = 0;
+        reset();
     }
     else if (guessesLeft == 0){
         losses++;
         document.querySelector('#losses').innerHTML = "Losses: " + losses;
         alert('You are not a Psychic! Try again');
-        guessesLeft = 9; 
-        guessesSoFar.length = 0;
+        reset();
         
     }
     else if (userGuess !== computerGuess){
+        debugger
         guessesLeft--;
-        document.querySelector('#let').innerHTML = "Your Guesses so far: " +
-        guessesSoFar.join(', ');
+        newGuessesSoFar();
+        
     }
+    updateGuessesLeft();
+   
     
 }
